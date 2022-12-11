c0ee5906d26e794faedfd789534a39e2e110ca29
subrato29 <subratosarkar29@gmai.com>
2022-12-11 14:06:12 -0500

    updated code

--- specs/crudApi.js
+++ specs/crudApi.js
@@ -1,11 +1,10 @@
 'use strict'
 
 import http from 'k6/http'
-import { sleep } from 'k6'
+import { check, sleep } from 'k6'
 import concurrency from '../config/concurrency.js'
 import endpoints from '../config/endpoints.js'
 import authToken from '../config/authToken.js'
-import { check } from 'k6'
 import requestBody from '../testData/requestBody.js'
 
 export const options = concurrency
--- specs/getApi.js
+++ specs/getApi.js
@@ -1,13 +1,16 @@
 'use strict'
 
 import http from 'k6/http'
-import { sleep } from 'k6'
+import { check, sleep } from 'k6'
 import concurrency from '../config/concurrency.js'
 import endpoints from '../config/endpoints.js'
 
 export const options = concurrency
 
 export default function () {
-    http.get (endpoints.getAllUsers());
+    const res = http.get (endpoints.getAllUsers());
+    check(res, {
+        'is get status 200': (res) => res.status == 200
+    })
     sleep (1);
 }
