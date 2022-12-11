94837c644867546f27e0b8ff2c6f38502c8bf2e3
subrato29 <subratosarkar29@gmai.com>
2022-12-11 11:33:14 -0500

    updated code

--- specs/postApi.js
+++ specs/postApi.js
@@ -3,26 +3,13 @@ import { sleep } from 'k6'
 import concurrency from '../config/concurrency.js'
 import endpoints from '../config/endpoints.js'
 import authToken from '../config/authToken.js'
-import perfUtils from '../utils/perfUtils.js'
 import { check } from 'k6'
-
-const random = perfUtils.generateAlphaNumericNo(6);
-
-const email = random + '@email.com';
-const name = 'Perf_Test_' + random;
-const gender = 'male';
-const status = 'active';
+import requestBody from '../testData/requestBody.js'
 
 export const options = concurrency
 
 export default function () {
-    let requestBody = {
-        "name" :  name,
-        "gender" : gender,
-        "email" : email,
-        "status" : status
-    }
-    const res = http.post (endpoints.getAllUsers(), requestBody, {
+    const res = http.post (endpoints.getAllUsers(), requestBody.create, {
         headers: authToken,
     })
     check(res, {
---
+++ testData/requestBody.js
@@ -0,0 +1,22 @@
+'use strict'
+
+import perfUtils from '../utils/perfUtils.js'
+
+const random = perfUtils.generateAlphaNumericNo(6);
+
+const requestBody = {
+    create: {
+        name :  'Perf_Test_' + random,
+        gender : 'male',
+        email : random + '@email.com',
+        status : 'active'
+    },
+    update: {
+        name :  'Perf_Test_' + random + '_UPDATED',
+        gender : 'male',
+        email : random + '@email.com',
+        status : 'active'
+    }
+}
+
+module.exports = requestBody;
