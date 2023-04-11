efa4e203937e5c694060a6ec1b0808351c634807
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-04-11 15:56:06 -0400

    updated prettier

--- utils/perfUtils.js
+++ utils/perfUtils.js
@@ -9,7 +9,7 @@ var perfUtils = {
   generateRandomInt: (length) => {
     return Math.floor(
       Math.pow(10, length - 1) +
-        Math.random() * (Math.pow(10, length) - Math.pow(10, length - 1) - 1),
+        Math.random() * (Math.pow(10, length) - Math.pow(10, length - 1) - 1)
     )
   },
 
